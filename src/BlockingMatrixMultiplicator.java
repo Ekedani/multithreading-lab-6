6f2cb11e42232c367b750e4bc8ce64c79fe0ceca
Vladlen <ilusenkovladlen@gmail.com>
2023-05-08 17:50:16 +0300

    Implemented BlockingMatrixMultiplicator, refactoring needed

--- src/BlockingMatrixMultiplicator.java
+++ src/BlockingMatrixMultiplicator.java
@@ -1,16 +1,17 @@
 import mpi.MPI;
 
 public class BlockingMatrixMultiplicator {
-    public static int NRA = 62;
-    public static int NCA = 15;
-    public static int NCB = 7;
+    public static int NRA = 10;
+    public static int NCA = 10;
+    public static int NCB = 10;
     public static int MASTER = 0;
     public static int FROM_MASTER = 1;
     public static int FROM_WORKER = 2;
 
     public static void main(String[] args) {
+        int averow, extra;
+        int[] rows = {0}, offset = {0};
         int taskId, tasksNumber, workersNumber;
-        int averow, extra, offset, rows;
 
         double[][] a = new double[NRA][NCA];
         double[][] b = new double[NCA][NCB];
@@ -34,30 +35,40 @@ public class BlockingMatrixMultiplicator {
 
             averow = NRA / workersNumber;
             extra = NRA % workersNumber;
-            offset = 0;
 
             for (int destination = 1; destination <= workersNumber; destination++) {
-                rows = (destination <= extra) ? averow + 1 : averow;
+                rows[0] = (destination <= extra) ? averow + 1 : averow;
                 MPI.COMM_WORLD.Send(offset, 0, 1, MPI.INT, destination, FROM_MASTER);
                 MPI.COMM_WORLD.Send(rows, 0, 1, MPI.INT, destination, FROM_MASTER);
-                MPI.COMM_WORLD.Send(offset, offset, rows, MPI.DOUBLE, destination, FROM_MASTER);
-                MPI.COMM_WORLD.Send(offset, offset, 1, MPI.DOUBLE, destination, FROM_MASTER);
-                offset += rows;
+                MPI.COMM_WORLD.Send(a, offset[0], rows[0], MPI.OBJECT, destination, FROM_MASTER);
+                MPI.COMM_WORLD.Send(b, 0, NCA, MPI.OBJECT, destination, FROM_MASTER);
+                offset[0] += rows[0];
             }
             for (int source = 1; source <= workersNumber; source++) {
+                MPI.COMM_WORLD.Recv(offset, 0, 1, MPI.INT, source, FROM_WORKER);
+                MPI.COMM_WORLD.Recv(rows, 0, 1, MPI.INT, source, FROM_WORKER);
+                MPI.COMM_WORLD.Recv(c, offset[0], rows[0], MPI.OBJECT, source, FROM_WORKER);
 
             }
             System.out.println("===== RESULT MATRIX =====");
             Helper.outputMatrix(c);
         } else {
-            /*for (int i = 0; i < NCB; i++) {
-                for (int j = 0; j < buff[0]; j++) {
-                    c[i][k] = 0;
-                    for (int k = 0; k < NCA; k++) {
-                        c[i][k] = c[i][k] + a[i][j] * b[j][k];
+            MPI.COMM_WORLD.Recv(offset, 0, 1, MPI.INT, MASTER, FROM_MASTER);
+            MPI.COMM_WORLD.Recv(rows, 0, 1, MPI.INT, MASTER, FROM_MASTER);
+            MPI.COMM_WORLD.Recv(a, 0, rows[0], MPI.OBJECT, MASTER, FROM_MASTER);
+            MPI.COMM_WORLD.Recv(b, 0, NCA, MPI.OBJECT, MASTER, FROM_MASTER);
+
+            for (int k = 0; k < NCB; k++) {
+                for (int i = 0; i < rows[0]; i++) {
+                    for (int j = 0; j < NCA; j++) {
+                        c[i][k] += a[i][j] * b[j][k];
                     }
                 }
-            }*/
+            }
+
+            MPI.COMM_WORLD.Send(offset, 0, 1, MPI.INT, MASTER, FROM_WORKER);
+            MPI.COMM_WORLD.Send(rows, 0, 1, MPI.INT, MASTER, FROM_WORKER);
+            MPI.COMM_WORLD.Send(c, 0, rows[0], MPI.OBJECT, MASTER, FROM_WORKER);
         }
         MPI.Finalize();
     }
